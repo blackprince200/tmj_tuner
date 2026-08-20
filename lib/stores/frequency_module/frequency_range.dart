db56df38a0c2cebd2fbea4eb9bb3a2cf1a1af839
Vikash-S-code <vksl13923@gmail.com>
2026-08-20 15:34:02 +0530

    --frequency data range added to the frequency model for default guitar and guitar sharpness related data also added

--- lib/stores/frequency_module/frequency_range.dart
+++ lib/stores/frequency_module/frequency_range.dart
@@ -1,5 +1,13 @@
 import './rangeId.dart';
 
 const ranges=<IdRange>[
+  IdRange(start: 82.17, end: 82.65, name: "Low E", note: "E2", targetHz: 82.41),
+  IdRange(start: 109.68, end: 110.32, name: "A", note: "A2", targetHz: 110.00),
+  IdRange(start: 146.41, end: 147.25, name: "D", note: "D3", targetHz: 146.83),
+  IdRange(start: 195.43, end: 196.57, name: "G", note: "G3", targetHz: 196.00),
+  IdRange(start: 246.23, end: 247.66, name: "B", note: "B3", targetHz: 246.94),
+  IdRange(start: 328.68, end: 330.58, name: "High E", note: "E4", targetHz:329.63)
+];
 
-]
+
+
--- lib/stores/frequency_module/rangeId.dart
+++ lib/stores/frequency_module/rangeId.dart
@@ -1,11 +1,24 @@
 class IdRange {
-  final int start;
-  final int end;
-  final String prefix;
+  final double start;
+  final double end;
+  final String name;
+  final String note;
+  final double targetHz;
+
+
+  final int startCents;
+  final int bestCents;
+  final int endCents;
+
 
   const IdRange({
     required this.start,
     required this.end,
-    required this.prefix,
+    required this.name,
+    required this.note,
+    required this.targetHz,
+    this.startCents=-5,
+    this.bestCents=0,
+    this.endCents=5
   });
 }
